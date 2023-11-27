ee5620276ef255e56824ecf80d9bcb63d4a92de0
wylliams <76174006+Kozato01@users.noreply.github.com>
2023-11-27 11:55:33 -0300

    Add files via upload
    #1

---
+++ GOTY2023/app.py
@@ -0,0 +1,48 @@
+import streamlit as st
+from camp import exibir_formulario, exibir_formulario_exclusao, exibir_formulario_visualizacao_respostas
+
+def definir_estilo_pagina():
+    estilo = """
+        <style>
+            .stApp > header {
+                background-color: transparent;
+            }
+
+            .stApp {
+                margin: auto;
+                font-family: -apple-system, BlinkMacSystemFont, sans-serif;
+                overflow: auto;
+                background: linear-gradient(135deg, #011326 25%, #ff851b 75%);
+                background-size: cover;
+                background-attachment: fixed;
+                color: #ffffff; /* Cor do texto */
+            }
+        </style>
+    """
+    st.markdown(estilo, unsafe_allow_html=True)
+
+def main():
+    st.set_page_config(
+        page_title="GOTY 2023 - Formulário",
+        page_icon=":trophy:",
+        layout="wide",
+        initial_sidebar_state="expanded",
+    )
+    definir_estilo_pagina()
+
+    # Adicionar links para páginas
+    st.sidebar.markdown("<h3 style='color: #ffffff;'>Navegação:</h3>", unsafe_allow_html=True)
+    page = st.sidebar.radio("", ["Formulário", "Visualizar Respostas", "Excluir Dados"], key="sidebar")
+
+    if page == "Formulário":
+        exibir_formulario()
+
+    elif page == "Visualizar Respostas":
+        exibir_formulario_visualizacao_respostas()
+        pass
+
+    elif page == "Excluir Dados":
+        exibir_formulario_exclusao()
+
+if __name__ == "__main__":
+    main()
